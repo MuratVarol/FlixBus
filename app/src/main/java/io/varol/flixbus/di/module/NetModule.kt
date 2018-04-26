bd37f5b058b73ab8e5e1fdc2172dba81b3a65e6c
varol <muratv@anadolu.edu.tr>
2018-04-26 17:35:06 +0300

    comments added

--- app/src/main/java/io/varol/flixbus/di/module/NetModule.kt
+++ app/src/main/java/io/varol/flixbus/di/module/NetModule.kt
@@ -7,6 +7,7 @@ import com.squareup.moshi.Rfc3339DateJsonAdapter
 import dagger.Module
 import dagger.Provides
 import io.varol.flixbus.BuildConfig
+import io.varol.flixbus.util.network.ConnectivityInterceptor
 import okhttp3.Cache
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
@@ -31,17 +32,22 @@ class NetModule {
         private const val CLIENT_CACHE_SIZE = 10 * 1024 * 1024L
         private const val CLIENT_CACHE_DIRECTORY = "http"
 
-        private val BASE_URL = "http://api.mobile.staging.mfb.io "
+        private const val BASE_URL = "http://api.mobile.staging.mfb.io"
 
     }
 
+    /**
+     * Provides Cache object for OkHttp
+     */
     @Provides
     @Singleton
-    fun providesCache(application: Application): Cache
-            = Cache(File(application.cacheDir,
+    fun providesCache(application: Application): Cache = Cache(File(application.cacheDir,
             CLIENT_CACHE_DIRECTORY),
             CLIENT_CACHE_SIZE)
 
+    /**
+     * Provides Moshi for Json parsing
+     */
     @Provides
     @Singleton
     fun providesMoshi(): Moshi {
@@ -50,12 +56,19 @@ class NetModule {
                 .build()
     }
 
+    /**
+     * Provides modified OkHttp Client builder.
+     *  logging network requests and responses
+     *  Checks if there is no internet connection
+     *  Sets timeouts for requests and response wait
+     */
     @Provides
     @Singleton
-    fun providesOkHttpClient(cache: Cache): OkHttpClient {
+    fun providesOkHttpClient(cache: Cache, context: Application): OkHttpClient {
         return OkHttpClient.Builder()
                 .addInterceptor(HttpLoggingInterceptor()
                         .setLevel(if (BuildConfig.DEBUG) Level.BODY else Level.NONE))
+                .addInterceptor(ConnectivityInterceptor(context.applicationContext))
                 .connectTimeout(CLIENT_TIME_OUT, TimeUnit.SECONDS)
                 .writeTimeout(CLIENT_TIME_OUT, TimeUnit.SECONDS)
                 .readTimeout(CLIENT_TIME_OUT, TimeUnit.SECONDS)
@@ -63,6 +76,10 @@ class NetModule {
                 .build()
     }
 
+
+    /**
+     * Retrofit builder for Service communications
+     */
     @Provides
     @Singleton
     fun providesRetrofit(okHttpClient: OkHttpClient): Retrofit =
@@ -72,7 +89,6 @@ class NetModule {
                     .addConverterFactory(MoshiConverterFactory.create(providesMoshi()))
                     .addCallAdapterFactory(CoroutineCallAdapterFactory())
                     .build()
-
 
 
 }
