05e778bd7c30337be7183bbca4025130553f4ba0
varol <muratv@anadolu.edu.tr>
2018-04-26 20:07:25 +0300

    custom scope added for dagger

---
+++ app/src/main/java/io/varol/flixbus/app/scope/PerActivity.kt
@@ -0,0 +1,14 @@
+package io.varol.flixbus.app.scope
+
+import javax.inject.Scope
+
+/**
+ * Created by varol on 26.4.2018.
+ */
+
+/**
+ * Custom scope for Dagger 2
+ */
+@Scope
+@Retention
+annotation class PerActivity
