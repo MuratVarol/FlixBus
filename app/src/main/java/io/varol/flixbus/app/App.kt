944ab4b583d2071178fa3afe9eedf538021d1be3
varol <muratv@anadolu.edu.tr>
2018-04-26 16:29:26 +0300

    App class created and inherited from MultiDexApplication

---
+++ app/src/main/java/io/varol/flixbus/app/App.kt
@@ -0,0 +1,17 @@
+package io.varol.flixbus.app
+
+import android.support.multidex.MultiDexApplication
+
+/**
+ * Created by varol on 26.4.2018.
+ */
+
+
+open class App : MultiDexApplication()
+{
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+
+}
